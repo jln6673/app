293cb9eacaee9e8f9c9f27e381ac46b3008f5388
aaaa <17633946673@163.com>
2021-04-28 11:20:19 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import rem from './assets/js/rem' // 引入rem.js
 import './assets/css/reset.css' // 引入样式清空css
-import Axios from 'axios' // 引入axios插件
 
 Vue.prototype.axios = Axios // 把数据请求的axios方法绑定在vue的原型对象上
 Vue.use(rem)
