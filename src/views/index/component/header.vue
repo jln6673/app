a9c466b5b6df3939a9baba7c8c509d124d427b64
aaaa <17633946673@163.com>
2021-04-29 08:44:39 +0800

    tijiao

---
+++ src/views/index/component/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <van-row>
+      <van-col span="8">
+        
+      </van-col>
+      <van-col span="8">
+        
+      </van-col>
+      <van-col span="8">
+        
+      </van-col>
+    </van-row>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
