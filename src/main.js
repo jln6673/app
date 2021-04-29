8bea69ec5041058a0e5e581f68c3fe8b42a24a84
aaaa <17633946673@163.com>
2021-04-29 11:21:12 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -7,11 +7,12 @@ import './assets/css/reset.css' // 引入样式清空css
 import Axios from 'axios' // 引入axios插件
 import Vant from 'vant' // 引入vant插件
 import 'vant/lib/index.css' // 引入vant插件所需CSS
-
+import swiper from 'vue-awesome-swiper'
+import 'swiper/swiper-bundle.css'
 Vue.prototype.axios = Axios // 把数据请求的axios方法绑定在vue的原型对象上
 Vue.use(rem)
 Vue.use(Vant)
-
+Vue.use(swiper)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,22 +1,73 @@
 <template>
   <div>
     首页
+    <!-- banner -->
+    <van-swipe class="my-swipe"  indicator-color="white">
+      <van-swipe-item class="lis" v-for="item in bannerList" :key="item.encodeId">
+        <img :src="item.imageUrl">
+      </van-swipe-item>
+    </van-swipe>
+    <!-- 圆形图标入口 -->
+    <swiper class="swiper" :options="swiperOption">
+      <swiper-slide
+      v-for="item in goList"
+      :key="item.id"
+      >
+        <div class="yuan">
+          <img :src="item.iconUrl">
+          <p>{{item.name}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 <script>
 export default {
   name: 'Index',
+  data () {
+    return {
+      bannerList: '',
+      goList: '',
+      swiperOption: {
+        slidesPerView: 5,
+        spaceBetween: 10,
+        freeMode: true
+      }
+    }
+  },
   methods: {
-    getdata () {
-      this.axios.get('').then((res) => {
+    getbanner () {
+      this.axios.get('/api/banner').then((res) => {
         console.log(res)
+        if (res.status === 200) {
+          this.bannerList = res.data.banners
+        }
       }).catch((err) => {
         console.log(err)
       })
+    },
+    getgo () {
+      this.axios.get('/api/homepage/dragon/ball').then((res) => {
+        console.log(res)
+        if (res.status === 200) {
+          this.goList = res.data.data
+        }
+      })
     }
+  },
+  mounted () {
+    this.getbanner()
+    this.getgo()
   }
 }
 </script>
 <style lang="stylus" scoped>
-
+.my-swipe
+  width 90%
+  margin 0 auto
+  .lis
+    img
+      width 100%
+      height 3.3rem
+      border-radius .5rem
 </style>
